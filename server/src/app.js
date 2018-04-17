5ae6daa4654985da9d53e50f913eaa5043ac3f14
Priyesh Gohil <priyesh114@hotmail.co.uk>
2018-04-17 22:59:17 +0100

    adding mongoose

--- server/src/app.js
+++ server/src/app.js
@@ -2,14 +2,22 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
+const mongoose = require('mongoose')
 
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
+mongoose.connect('mongodb://localhost:27017/')
+
 app.get('/status', (req, res) => {
   res.send({msg: 'hello world'})
+})
+
+app.post('/admin', (req, res) => {
+  console.log(req.body)
+  res.send({msg: 'log in info sent'})
 })
 
 app.listen(process.env.PORT || 8081)
