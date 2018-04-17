ddc3ddce7d57a61ba655622bf5c5c5daa6ea21ca
Priyesh Gohil <priyesh114@hotmail.co.uk>
2018-04-17 19:36:33 +0100

    client side validation for admin login form

--- client/src/Admin/LoginForm.js
+++ client/src/Admin/LoginForm.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react';
 import passwordIcon from './img/passwordIcon.svg';
 import userIcon from './img/userFormIcon.svg'
+import crossIcon from './img/crossIcon.svg'
 class LoginForm extends Component {
   
   state = {
-    email: '',
-    password: ''
+    username: '',
+    usernameError: '',
+    password: '',
+    passwordError: ''
+  }
+
+  validate = () => {
+    //reset
+    let hasErrors = false;
+    
+    this.setState({
+      usernameError: '',
+      passwordError: ''
+    });
+
+    if(this.state.username.length === 0) {
+      hasErrors = true;
+      this.setState({usernameError: 'please enter a valid username'});
+    }
+
+    if(this.state.password.length === 0) {
+      hasErrors = true;
+      this.setState({passwordError: 'please enter a valid password'});
+    }
+
+    return hasErrors;
+  }
+    
+  submit = e => {
+    e.preventDefault();
+    
+    if(!this.validate()) {
+      console.log(this.state);
+    }
   }
 
 
@@ -14,17 +47,16 @@ class LoginForm extends Component {
       <form className='adminLoginForm'>
         <div className='textInput'>
         <div className='textInput__logo'><img width='25' height='25' src={userIcon} alt='Enter your user name'/></div>
-        <input value={this.state.email} onChange={e => this.setState({email: e.target.value})}/>
+        <input name='username' placeholder='Username' value={this.state.username} onChange={e => this.setState({username: e.target.value.trim()})}/>
+        {this.state.usernameError.length > 0 && <div className='formError'><img src={crossIcon} alt='error icon' /></div>}
         </div>
          
         <div className='textInput'>
           <div className='textInput__logo'><img width='25' height='25' src={passwordIcon} alt='Enter your password'/></div>
-          <input type='password' value={this.state.password} onChange={e => this.setState({password: e.target.value})}/>
-        </div>
-        
-        <br/>
-        
-        <button className='loginBtn'>Log In</button>
+          <input placeholder='Password' type='password' value={this.state.password} onChange={e => this.setState({password: e.target.value.trim()})}/>
+          {this.state.passwordError.length > 0 && <div className='formError'><img src={crossIcon} alt='error icon' /></div>}
+        </div>        
+        <button onClick={e => this.submit(e)} className='loginBtn'>Log In</button>
       </form>
     )
   }
