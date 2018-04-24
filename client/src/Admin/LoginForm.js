cb4edc8c86cc20e0f506d10572534484834e4fc5
Priyesh Gohil <priyesh114@hotmail.co.uk>
2018-04-24 20:55:44 +0100

    using the api to login in users

--- client/src/Admin/LoginForm.js
+++ client/src/Admin/LoginForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import passwordIcon from './img/passwordIcon.svg';
 import userIcon from './img/userFormIcon.svg'
 import crossIcon from './img/crossIcon.svg'
@@ -8,7 +9,8 @@ class LoginForm extends Component {
     username: '',
     usernameError: '',
     password: '',
-    passwordError: ''
+    passwordError: '',
+    serverError: ''
   }
 
   validate = () => {
@@ -17,7 +19,7 @@ class LoginForm extends Component {
     
     this.setState({
       usernameError: '',
-      passwordError: ''
+      passwordError: '',
     });
 
     if(this.state.username.length === 0) {
@@ -35,9 +37,23 @@ class LoginForm extends Component {
     
   submit = e => {
     e.preventDefault();
+
+    this.setState({serverError: ''});
     
     if(!this.validate()) {
-      console.log(this.state);
+      // Make a request for a user with a given ID
+      axios.post('http://localhost:8081/signin',{
+        username: this.state.username,
+        password: this.state.password
+      })
+      .then((response) => {
+        this.setState({serverError : 'Success!'});
+      })
+      .catch((error) => {
+        if(error.response.status === 500 || error.response.status === 401) {
+          this.setState({serverError : 'Please try again, your username or password is incorrect'});
+        }
+      });
     }
   }
 
@@ -45,6 +61,7 @@ class LoginForm extends Component {
   render() {
     return (
       <form className='adminLoginForm'>
+        {this.state.serverError.length > 0 && <div> {this.state.serverError} </div>}
         <div className='textInput'>
         <div className='textInput__logo'><img width='25' height='25' src={userIcon} alt='Enter your user name'/></div>
         <input name='username' placeholder='Username' value={this.state.username} onChange={e => this.setState({username: e.target.value.trim()})}/>
